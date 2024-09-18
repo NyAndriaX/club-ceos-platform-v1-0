abab3c1a584a9a2dbd9f9af688226a508997a95c
NyAndriaX <tsilavinaandriamahafaly01@gmail.com>
2024-09-18 10:04:37 +0300

    add:add not finished to pricing

--- src/app/(pages)/pricing/page.tsx
+++ src/app/(pages)/pricing/page.tsx
@@ -15,7 +15,25 @@ const Pricing = () => {
   return (
     <section className="container mx-4">
       <div className="flex flex-row items-start gap-4">
-        <Card className="min-w-80"></Card>
+        <Card
+          className="min-w-96"
+          title={
+            <div className="flex flex-col gap-1">
+              <h1 className="text-blue-950 font-extrabold text-4xl">€ 99,90</h1>
+              <div className="flex flex-row items-center justify-end gap-2 text-xs ">
+                <p className="text-end font-normal w-[8vw]">
+                  par mois et par assistance
+                </p>
+                <li className="pi pi-info-circle"></li>
+              </div>
+            </div>
+          }
+          subTitle={
+            <h1 className="text-blue-900 font-bold text-2xl">SOCIETE</h1>
+          }
+        >
+          <p></p>
+        </Card>
         <Card className="min-w-80 bg-blue-900"></Card>
       </div>
     </section>
