e613b5783741995f5e309c5f9f2441aefacc8948
Andriatahina-niaina <ramanitrarivomamitiana@gmail.com>
2024-10-01 04:43:26 -0400

    fix:update navbar

--- src/ui/Member/Home/homeView.tsx
+++ src/ui/Member/Home/homeView.tsx
@@ -4,9 +4,9 @@ import { Button } from "primereact/button";
 
 const HomeView = () => {
     return (
-        <main className="flex flex-col gap-4">
+        <main className="flex flex-col gap-4 ">
             <div className="flex flex-row gap-4 items-end justify-between">
-                <h2 className="text-3xl font-semibold">Bienvenue !</h2>
+                <h2 className="text-3xl font-semibold">welcome !</h2>
                 <Button label="Créer un nouveau sujet" size="small" outlined />
             </div>
         </main>
--- src/ui/common/components/layout/MemberLayout/MemberPage.tsx
+++ src/ui/common/components/layout/MemberLayout/MemberPage.tsx
@@ -1,15 +1,34 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, ReactNode } from "react";
 
-export const MemberPage: React.FC<PropsWithChildren<{ title: string }>> = ({
-  title,
-  children,
+
+interface MemberPageProps
+    extends Omit<React.HTMLAttributes<HTMLDivElement>, "title"> {
+    title: string | ReactNode;
+    subtitle?: string | ReactNode;
+}
+
+export const MemberPage: React.FC<PropsWithChildren<MemberPageProps>> = ({
+    title,
+    subtitle,
+    children,
+    className,
+    ...props
 }) => {
-  return (
-    <div className="flex flex-col mx-auto gap-4 lg:px-8 lg:py-4">
-      <h1 className="text-lg md:text-2xl font-bold w-[98vw] lg:w-[70vw]">
-        {title}
-      </h1>
-      <div>{children}</div>
-    </div>
-  );
+    return (
+        <div
+            className={`flex flex-col mx-auto gap-4 lg:px-8 lg:py-4 h-full items-start  ${className ?? "w-full"
+                }`}
+            {...props}
+        >
+            <div className="flex flex-col gap-2 items-start">
+                <h1 className="text-lg md:text-2xl font-bold w-full">{title}</h1>
+                {subtitle && (
+                    <p className="text-base font-light text-gray-500 w-2/3">{subtitle}</p>
+                )}
+            </div>
+            <div className="flex flex-col gap-4 mt-4 max-h-full overflow-auto w-full">
+                {children}
+            </div>
+        </div>
+    );
 };
