3ae42d8641699c4a00d7e7dac69982fc77232842
Andriatahina-niaina <ramanitrarivomamitiana@gmail.com>
2024-10-02 03:59:55 -0400

    fix:positiob interest center

---
+++ src/app/member/interest-center/page.tsx
@@ -0,0 +1,9 @@
+import { InterestCenterView } from "@/ui/Member/InterestCenter/interestCenterView"
+
+const InterestCenter = () => {
+    return (
+        <InterestCenterView />
+    )
+}
+
+export default InterestCenter
--- src/ui/Member/Topics/interestCenter/interestCenter.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { MemberPage } from "@/ui/common/components/layout/MemberLayout/MemberPage";
-
-export const InterestCenterView = () => {
-    return (<MemberPage title="Vos Interets"></MemberPage>);
-}
