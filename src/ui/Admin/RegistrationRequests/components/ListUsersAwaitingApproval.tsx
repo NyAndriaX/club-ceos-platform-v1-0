51c8b0690a0bb55ef71cd93816b43c393b8dd424
NyAndriaX <tsilavinaandriamahafaly01@gmail.com>
2024-10-04 07:02:51 +0300

    fix:view infos registration requests

--- src/ui/Admin/RegistrationRequests/components/ListUsersAwaitingApproval.tsx
+++ src/ui/Admin/RegistrationRequests/components/ListUsersAwaitingApproval.tsx
@@ -9,130 +9,129 @@ import { useLocalStorage } from "primereact/hooks";
 import { ProgressSpinner } from "primereact/progressspinner";
 
 export function ListUsersAwaitingApproval() {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [usersAwaitingApprovals, setUsersAwaitingApprovals] = useState<
-    UserOutput[] | []
-  >([]);
-  const [, setUsersAwaitingApproval] = useLocalStorage<UserOutput[] | []>(
-    [],
-    "usersAwaitingApproval"
-  );
-
-  const onCarouselClick = (userId: number) => {
-    router.push(
-      `/admin/registration-requests/${userId}?action=usersAwaitingApproval`
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [usersAwaitingApprovals, setUsersAwaitingApprovals] = useState<
+        UserOutput[] | []
+    >([]);
+    const [, setUsersAwaitingApproval] = useLocalStorage<UserOutput[] | []>(
+        [],
+        "usersAwaitingApproval"
     );
-  };
 
-  useEffect(() => {
-    async function getUsersAwaitingApproval() {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          "/api/admin?action=getUsersAwaitingApproval"
-        );
-        const { users } = await response.json();
+    const onCarouselClick = (userId: number) => {
+        window.location.href = `/admin/registration-requests/${userId}?action=usersAwaitingApproval`
 
-        setUsersAwaitingApprovals(users);
-        setUsersAwaitingApproval(users);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    getUsersAwaitingApproval();
-  }, []);
+    };
 
-  const responsiveOptions = [
-    {
-      breakpoint: "1400px",
-      numVisible: 4,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "1199px",
-      numVisible: 3,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "767px",
-      numVisible: 2,
-      numScroll: 1,
-    },
-    {
-      breakpoint: "575px",
-      numVisible: 1,
-      numScroll: 1,
-    },
-  ];
+    useEffect(() => {
+        async function getUsersAwaitingApproval() {
+            setIsLoading(true);
+            try {
+                const response = await fetch(
+                    "/api/admin?action=getUsersAwaitingApproval"
+                );
+                const { users } = await response.json();
 
-  const cardUsersTemplate = (
-    usersAwaitingApprovals: UserOutput,
-    onCarouselClick: (userId: number) => void
-  ) => {
-    return (
-      <div
-        className="relative flex flex-col border border-gray-300 border-round rounded-md m-2 text-center py-5 px-3 max-w-[300px] transition hover:bg-gray-300 cursor-pointer"
-        onClick={() => onCarouselClick(usersAwaitingApprovals.id)}
-      >
-        <div className="flex flex-col items-center justify-center mb-3">
-          <Image
-            src="/profil.jpeg"
-            alt="anonymous profil"
-            width={200}
-            height={200}
-            className="h-20 w-20 shadow-sm object-cover rounded-full"
-          />
-          <h4 className="mb-1 mt-4 font-normal">
-            Nom : {usersAwaitingApprovals.lastName}{" "}
-            {usersAwaitingApprovals.firstName}.
-          </h4>
-          <h6>Chiffre d&apos;affaire : {usersAwaitingApprovals.revenue} €.</h6>
-          {usersAwaitingApprovals.isNew && (
-            <Badge
-              severity="danger"
-              value="nouveau"
-              className="absolute -top-2 -right-6 z-50"
-            />
-          )}
-        </div>
-      </div>
-    );
-  };
-  return (
-    <Card className="shadow-sm">
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-4">
-          <h2 className="text-base font-semibold">
-            Liste d&apos;inscription en attente de validation
-          </h2>
-          <p className="text-blue-500 font-semibold mb-4 text-xs transition hover:underline">
-            N.B: Veuillez sélectionner un utilisateur dont la validation est en
-            attente.
-          </p>
-        </div>
-
-        {isLoading ? (
-          <ProgressSpinner
-            style={{ width: "50px", height: "50px" }}
-            strokeWidth="4"
-            fill="var(--surface-ground)"
-          />
-        ) : (
-          <Carousel
-            value={usersAwaitingApprovals ?? []}
-            numScroll={1}
-            numVisible={3}
-            className="flex flex-row gap-4 items-center"
-            responsiveOptions={responsiveOptions}
-            itemTemplate={(usersAwaitingApprovals) =>
-              cardUsersTemplate(usersAwaitingApprovals, onCarouselClick)
+                setUsersAwaitingApprovals(users);
+                setUsersAwaitingApproval(users);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setIsLoading(false);
             }
-          />
-        )}
-      </div>
-    </Card>
-  );
+        }
+        getUsersAwaitingApproval();
+    }, []);
+
+    const responsiveOptions = [
+        {
+            breakpoint: "1400px",
+            numVisible: 4,
+            numScroll: 1,
+        },
+        {
+            breakpoint: "1199px",
+            numVisible: 3,
+            numScroll: 1,
+        },
+        {
+            breakpoint: "767px",
+            numVisible: 2,
+            numScroll: 1,
+        },
+        {
+            breakpoint: "575px",
+            numVisible: 1,
+            numScroll: 1,
+        },
+    ];
+
+    const cardUsersTemplate = (
+        usersAwaitingApprovals: UserOutput,
+        onCarouselClick: (userId: number) => void
+    ) => {
+        return (
+            <div
+                className="relative flex flex-col border border-gray-300 border-round rounded-md m-2 text-center py-5 px-3 max-w-[300px] transition hover:bg-gray-300 cursor-pointer"
+                onClick={() => onCarouselClick(usersAwaitingApprovals.id)}
+            >
+                <div className="flex flex-col items-center justify-center mb-3">
+                    <Image
+                        src="/profil.jpeg"
+                        alt="anonymous profil"
+                        width={200}
+                        height={200}
+                        className="h-20 w-20 shadow-sm object-cover rounded-full"
+                    />
+                    <h4 className="mb-1 mt-4 font-normal">
+                        Nom : {usersAwaitingApprovals.lastName}{" "}
+                        {usersAwaitingApprovals.firstName}.
+                    </h4>
+                    <h6>Chiffre d&apos;affaire : {usersAwaitingApprovals.revenue} €.</h6>
+                    {usersAwaitingApprovals.isNew && (
+                        <Badge
+                            severity="danger"
+                            value="nouveau"
+                            className="absolute -top-2 -right-6 z-50"
+                        />
+                    )}
+                </div>
+            </div>
+        );
+    };
+    return (
+        <Card className="shadow-sm">
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-4">
+                    <h2 className="text-base font-semibold">
+                        Liste d&apos;inscription en attente de validation
+                    </h2>
+                    <p className="text-blue-500 font-semibold mb-4 text-xs transition hover:underline">
+                        N.B: Veuillez sélectionner un utilisateur dont la validation est en
+                        attente.
+                    </p>
+                </div>
+
+                {isLoading ? (
+                    <ProgressSpinner
+                        style={{ width: "50px", height: "50px" }}
+                        strokeWidth="4"
+                        fill="var(--surface-ground)"
+                    />
+                ) : (
+                    <Carousel
+                        value={usersAwaitingApprovals ?? []}
+                        numScroll={1}
+                        numVisible={3}
+                        className="flex flex-row gap-4 items-center"
+                        responsiveOptions={responsiveOptions}
+                        itemTemplate={(usersAwaitingApprovals) =>
+                            cardUsersTemplate(usersAwaitingApprovals, onCarouselClick)
+                        }
+                    />
+                )}
+            </div>
+        </Card>
+    );
 }
